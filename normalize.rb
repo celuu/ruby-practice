6b43e0acf4b8ae230c4a635366c3226af461a4f2
celuu <christine.e.luu@gmail.com>
2026-02-17 15:05:40 -0800

    Merge

--- normalize.rb
+++ normalize.rb
@@ -11,15 +11,21 @@ events = [
 
 
 def merge_users(users, events)
-  users.map do |user|
+  normalized_users = users.map do |user|
     user.merge(id: user[:id].to_i)
   end
 
-  events.map do |event|
-    event.merge(id: event[:user_id].to_i)
+  normalized_events = events.map do |event|
+    event.merge(user_id: event[:user_id].to_i)
   end
-  
 
+  events_by_user = normalized_events.each_with_object(Hash.new{|h,k| h[k] = []}) do |event, hash|
+    hash[event[:user_id]] << event[:type]
+  end
+
+  normalized_users.map do |user|
+    user.merge(events: events_by_user[user[:id]])
+  end
 end
 
 
